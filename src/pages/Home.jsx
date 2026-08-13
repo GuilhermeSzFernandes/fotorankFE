9e272848b32c54ede028253391bf0e2979f0cbcd
Guilherme Fernandes <guilhermenovo3001@gmail.com>
2026-08-13 18:11:37 -0300

    Fix: participantes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const INFO = [
   },
   {
     label: 'Quem pode participar',
-    detail: 'Todos os moradores de Carapicuíba',
+    detail: 'Alunos e ex-alunos da ETEC',
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
         <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
